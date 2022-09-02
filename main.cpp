35fc48e0b1462e5211544a85f6bf5908a0cc4242
Deivvvv <52557350+Deivvvv@users.noreply.github.com>
2022-09-02 14:21:52 +0500

    v2

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-   // Ui::MainWindow caller;
-
+    MainWindow w ;
+    //QMainWindow caller;
+  //  Ui::MainWindow caller;
+  //  caller.setupUi(&w);
+  //  w.line1 = caller.lineEdit_3;
+  //  w.line2 = caller.lineEdit_3;
+  //  w.line3 = caller.lineEdit_3;
 
     w.show();
     return a.exec();
--- mainwindow.h
+++ mainwindow.h
@@ -2,9 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <string>
+#include <QString>
 #include <iostream>
 
+#include <QString>
 #include <QtWidgets/QLineEdit>
 
 QT_BEGIN_NAMESPACE
@@ -17,25 +18,93 @@ class MainWindow : public QMainWindow
 
 public:
 
+    bool first;
+    QString a ="",b="";
     QLineEdit* line1 = nullptr;
     QLineEdit* line2 = nullptr;
-    QLineEdit* line3 = nullptr;
 
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 public slots:
+    double returnDouble(bool first){
+        QStirng str ="";
+        if(first)
+            str = a;
+        else
+            str = b;
+        //(first)? a:b;
+        return str.toDouble();
+    };
+
     void plus(){
-        int a = line1->text().split(" ")[0].toInt();
-        int b = line2->text().split(" ")[0].toInt();
-        line3->setText("" + (a+b));
+        double da = returnDouble(true),db= returnDouble(false);
+        da += db;
+        a = "" +da;
+      //  a +=b;
+        b="";
+        result();
     };
     void minus(){
-        std::cout <<  "Call";
+       // a -=b;
+        b="";
+        result();
     };
-    void division(){};
-    void multiplication(){};
+    void division(){
+        if(b == ""){
+            line2->setText("Error");
+        }else{
+         //   a/=b;
+          //  b=0;
+          //  result();
+        }
 
+    };
+    void multiplication(){
+       // a*=b;
+        b="";
+        result();
+    };
+    void clear(){
+        (first) ? a="":b="";
+        result();
+    };
+    void result(){
+        if(a == "")
+            line1->setText("0");
+        else
+            line1->setText(a);
+        if(b == "")
+            line2->setText("0");
+        else
+            line2->setText(b);
+    };
+    void add(int d){
+        //QString str = ((QPushButton*)sender())->text();
+       // int d = str.split(" ")[0].toInt();
+        if(first)
+            a+=""+d;
+        else
+            b+=""+d;
+
+        result();
+    };
+    void select1(){
+        first =true;
+    };
+    void select2(){
+        first =false;
+    };
+    void add0(){add(0);};
+    void add1(){add(1);};
+    void add2(){add(2);};
+    void add3(){add(3);};
+    void add4(){add(4);};
+    void add5(){add(5);};
+    void add6(){add(6);};
+    void add7(){add(7);};
+    void add8(){add(8);};
+    void add9(){add(9);};
 
 private:
     Ui::MainWindow *ui;
