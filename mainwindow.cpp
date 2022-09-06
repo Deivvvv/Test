4ef5e304f90889b35448bd48891adddf98aed7e1
Deivvvv <52557350+Deivvvv@users.noreply.github.com>
2022-09-06 16:43:30 +0500

    tv v1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,9 +7,8 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    line1 = ui->lineEdit;
-    line2 = ui->lineEdit_2;
-    line3 = ui->lineEdit_3;
+    line1 = ui->lineEdit_2;
+    line2 = ui->lineEdit_3;
 
 
    // connect(ui->pushButton, SIGNAL(cliked()),SLOT(add0())));
--- mainwindow.h
+++ mainwindow.h
@@ -19,102 +19,54 @@ class MainWindow : public QMainWindow
 
 public:
 
-    int mood =0;
+    int cn =0, v=0;
 
     QLineEdit* line1 = nullptr;
     QLineEdit* line2 = nullptr;
-    QLineEdit* line3 = nullptr;
 
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 public slots:
-    double returnDouble(bool first){
-        //stirng str ="";
-        if(first)
-            return line1->text().toDouble();
-
-        return line2->text().toDouble();
+    void readCN(){
+        line2->setText("CN " +QString::number(cn));
+       // line2->setText("CN "+cn);
+    };
+    void readV(){
+       // std::string str ="Volume "+QString::number(v) +"0%";
+       // line1->setText("Volume ");
+        line1->setText( "Volume "+QString::number(v) +"0%");
+    };
+    void selectCN(){
+       QPushButton *button = (QPushButton *)sender();
+       cn = button->text().toInt();
+       readCN();
+    };
+    void CNPlus(){
+       if(cn >=30)
+           cn=0;
+       else
+           cn++;
+       readCN();
+    };
+    void CNMinus(){
+       if(cn <=0)
+           cn=30;
+       else
+           cn--;
+       readCN();
+    };
+    void VolumePlus(){
+       if(v <=9)
+           v++;
+       readV();
+    };
+    void VolumeMinus(){
+       if(v>0)
+           v--;
+       readV();
     };
 
-    void plus(){
-        mood =0;
-        switchMood();
-    };
-    void minus(){
-        mood =1;
-        switchMood();
-    };
-
-    void division(){
-        mood =3;
-        switchMood();
-    };
-    void multiplication(){
-        mood =2;
-        switchMood();
-    };
-    void clear(){
-        if(line2->text() !="0"){
-            line2->setText("0");
-            return;
-        }
-
-        line1->setText("0");
-    };
-    void add(){
-        QPushButton *button = (QPushButton *)sender();
-        if(line2->text() =="Error" || line2->text() == "0")
-            line2->setText(button->text());
-        else
-            line2->setText(line2->text() +button->text());
-
-    };
-    void results(){
-        if(line2->text() =="Error" )
-            return;
-        double da = returnDouble(true),db= returnDouble(false);
-
-        switch(mood){
-        case(0):
-            da += db;
-            break;
-        case(1):
-            da -= db;
-            break;
-        case(2):
-            da *= db;
-            break;
-        case(3):
-            if(line2->text() =="0" ){
-                line2->setText("Error");
-                return;
-            }
-            da /= db;
-            break;
-        }
-
-
-        line1->setText(QString::fromStdString(std::to_string(da)));
-       // line1->setText("0");
-        line2->setText("0");
-    };
-    void switchMood(){
-        switch(mood){
-        case(0):
-             line3->setText("+");
-            break;
-        case(1):
-             line3->setText("-");
-            break;
-        case(2):
-             line3->setText("*");
-            break;
-        case(3):
-             line3->setText("/");
-            break;
-        }
-    };
 
 private:
     Ui::MainWindow *ui;
